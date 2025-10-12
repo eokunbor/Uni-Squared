45124166b2fce4f68aba70b273d7bd8eb6e16960
Elizabeth Okunbor <elizabethokunbor04@gmail.com>
2025-10-12 17:02:15 -0500

    Expo & JSX file Update

---
+++ Uni-Squared/app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
